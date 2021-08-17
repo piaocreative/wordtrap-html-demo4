5c49a423bdcaf55c4df10865dc9f757a01d4eb1a
piaocreative <johnpiao171019@gmail.com>
2021-08-17 13:26:20 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,79 @@
+$( document ).ready( function() {
+    
+    if ( $( '.logos' ).length ) {
+        $( '.logos' ).slick({
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            dots: false,
+            arrows: false,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                }
+            ]
+        });
+    }  
+
+    if ( $( '#product_categories' ).length ) {
+
+        $( '#product_categories' ).slick({
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            autoplay: false,            
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        infinite: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                }
+            ]
+        });
+
+    }
+
+
+} );
