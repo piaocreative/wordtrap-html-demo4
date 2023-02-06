36b56d886183c4fdbdc44b1a9967380aac09a2d7
piaocreative <johnpiao171019@gmail.com>
2023-02-06 08:29:26 -0800

    Updated JS

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $( document ).ready( function() {
 		$( '.logos' ).slick({
 			infinite: true,
 			slidesToShow: 5,
-			slidesToScroll: 5,
+			slidesToScroll: 1,
 			dots: false,
 			arrows: false,
 			responsive: [
@@ -27,7 +27,7 @@ $( document ).ready( function() {
 					settings: {
 						infinite: true,
 						slidesToShow: 4,
-						slidesToScroll: 4,
+						slidesToScroll: 1,
 					}
 				},
 				{
@@ -35,7 +35,7 @@ $( document ).ready( function() {
 					settings: {
 						infinite: true,
 						slidesToShow: 3,
-						slidesToScroll: 3,
+						slidesToScroll: 1,
 					}
 				},
 				{
@@ -43,7 +43,7 @@ $( document ).ready( function() {
 					settings: {
 						infinite: true,
 						slidesToShow: 2,
-						slidesToScroll: 2,
+						slidesToScroll: 1,
 					}
 				}
 			]
@@ -112,16 +112,23 @@ $( document ).ready( function() {
 
 	// QTY
 	if ( $('.quantity').length ) {
-		$('.quantity  #plus-btn').click(function(evt) {
-			evt.preventDefault();
-			$('.qty').val(parseInt($('.qty').val()) + 1 );
-		});
-		$('.quantity #minus-btn').click(function(evt) {
-			evt.preventDefault();
-			$('.qty').val(parseInt($('.qty').val()) - 1 );
-			if ($('.qty').val() == 0) {
-				$('.qty').val(1);
-			}
+		$('.quantity').each(function() {
+			var $this = $(this);
+			var $qty = $this.find('.qty');
+			var $plusBtn = $this.find('#plus-btn');
+			var $minusBtn = $this.find('#minus-btn');
+
+			$plusBtn.click(function(evt) {
+				evt.preventDefault();
+				$qty.val(parseInt($qty.val()) + 1 );
+			});
+			$minusBtn.click(function(evt) {
+				evt.preventDefault();
+				$qty.val(parseInt($qty.val()) - 1 );
+				if ($qty.val() == 0) {
+					$qty.val(1);
+				}
+			});
 		});
 	}
 
